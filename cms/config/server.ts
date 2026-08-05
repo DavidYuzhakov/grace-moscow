1b33509384b888b6282fce9e06a22c003e594841
DavidYuzhakov <davidyuzhakov17@gmail.com>
2026-08-05 21:55:44 +0300

    update server.ts

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -5,7 +5,11 @@ const config = ({
 }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
+  url: env('URL', 'http://localhost:1337'),
+
   proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
   },
