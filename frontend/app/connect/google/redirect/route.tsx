171ff3cf34178d0119e967857734472d6d36d208
DavidYuzhakov <davidyuzhakov17@gmail.com>
2026-08-09 23:19:31 +0300

    fix: update url redirect in oauth

--- frontend/app/connect/google/redirect/route.tsx
+++ frontend/app/connect/google/redirect/route.tsx
@@ -5,7 +5,8 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const accessToken = searchParams.get('access_token')
 
-  if (!accessToken) return NextResponse.redirect(new URL('/', request.url))
+  if (!accessToken)
+    return NextResponse.redirect(new URL('/', 'https://grace.moscow'))
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/auth/google/callback?access_token=${accessToken}`,
@@ -14,5 +15,5 @@ export async function GET(request: Request) {
 
   await setAuthToken(data.jwt)
 
-  return NextResponse.redirect(new URL('/', request.url))
+  return NextResponse.redirect(new URL('/', 'https://grace.moscow'))
 }
